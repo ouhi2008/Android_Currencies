cf084ea96b2d4d0c901f72f1ee0c89862cb1967b
Fei Wang <fei.wang@morningstar.com>
2016-02-24 15:09:55 +0800

    Unpacks currency codes from bundle.

--- app/src/main/java/com/apress/gerber/currencies/MainActivity.java
+++ app/src/main/java/com/apress/gerber/currencies/MainActivity.java
@@ -10,12 +10,16 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button mCalcButton;
     private TextView mConvertedTextView;
     private EditText mAmountEditText;
     private Spinner mForSpinner, mHomSpinner;
+    private String[] mCurrencies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +27,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //unpack ArrayList from the bundle and convert to array
+        ArrayList<String> arrayList=((ArrayList<String>)getIntent().getSerializableExtra(SplashActivity.KEY_ARRAYLIST));
+        Collections.sort(arrayList);
+        mCurrencies = arrayList.toArray(new String[arrayList.size()]);
 
         mConvertedTextView=(TextView)findViewById(R.id.txt_converted);
         mAmountEditText=(EditText)findViewById(R.id.edt_amount);
